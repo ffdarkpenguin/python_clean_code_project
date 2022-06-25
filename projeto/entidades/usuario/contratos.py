16586649a41fd3ac01c4f9caabe5406b41b05943
Flávio Fonseca <ff@darkpenguin.com.br>
2022-06-25 14:49:43 -0300

    unifica params, model e retorno num único arquivo

--- projeto/entidades/usuario/contratos.py
+++ projeto/entidades/usuario/contratos.py
@@ -4,6 +4,16 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class UsuarioParametros(BaseModel):
+    nome: str
+    fone: str
+
+
+class FiltroUsuarios(BaseModel):
+    nome: Optional[str] = None
+    fone: Optional[str] = None
+
+
 class Usuario(BaseModel):
     id: int
     nome: str
